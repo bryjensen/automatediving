127cc7863603e2b317f95901e5e6769c970b4ae1
Bryan Jensen <56618606+bryjensen@users.noreply.github.com>
2020-01-31 09:57:28 -0800

    Add files via upload

---
+++ diving6.py
@@ -0,0 +1,79 @@
+#what is first, look at the chambers
+#make a virtual chamber first, easy to change the dimensions
+# write out all the parts, its easy cause there is only a few
+# K.I.S.S.
+
+def main_chamber(constant, variable):
+    electrical = # needs to be measured in which way, constant_supply, battery_supply
+    heating/cooling = # central heating and cooling outside, larger volume to scrub
+    emergency_procedures = # real and virtual
+
+def transfer_chamber(constant, variable):
+    electrical = # needs to be measured in which way, constant_supply, battery_supply
+
+def hyperbaric_chamber(constant, variable):
+    electrical = # needs to be measured in which way, constant_supply, battery_supply
+
+def bell_chamber(constant, variable):
+    electrical = # needs to be measured in which way, constant_supply, battery_supply
+    # main_chamber(_a,b,c,...) for identification for added chambers
+    
+def reclaim_system(constant, variable):
+    bell_chamber # the machine learning and ai for this is what is going to make something
+    main_chamber # everything else will be based off of this, need to draw some math
+
+def bell_umbilical(surface, bell):
+    
+def oil_filled_box(electrical, connections):
+    
+def diver_hot_water(constant, variable):
+    water in box heat it up water out
+    
+def environmental_control(constants, variables):
+    
+def potable_water(constant, variables):
+    
+def hp_compressor(constant, variable):
+    
+def launch_and_recovery(constant, variable):
+    hydraulics = # sensors for the machine_learning and ai_interface
+    
+def cooling_water(constant, variable):
+    pump_a = online
+    pump_b = offline
+    # watch the flow, minute adjustment from any restriction
+    
+def gas_storage(max, variable):
+    helium_bottles = # how many at max press
+    oxygen_bottles = # how many at max press
+    treatment_bottles = # how many at max press
+    bell_bottles = # how many at max press
+    hyperbaric_bottles = # how many at max press
+    other_bottles = # how many at max press
+    more_bottles = # how many at max press
+    
+def gas_distribution(constants, variables):
+    electrical_valves, more like a bike inner tube, inside keeps it closed, outside
+    will push open to allow needed amount, one ways that prevent instantaneous release
+    , spring resistance
+    intricities for the ai to manage, how many amps/watts to keep the valve open at which
+    pressure, ability to manage a reducer 
+
+def machine_learning(constant, variable):
+    what is to be learned 
+    what is constant
+    what is variable
+    adjustments from outside set parameters, how to teach that 
+    
+def ai_interface(constant, variable):
+    teach the spam the why as to how the constant is maintained
+    avoid three_right_turns in snake
+    
+def touch_screen(real, virtual):
+    # the interface what is going to be displayed for whom
+
+list = ['main_chamber','transfer_chamber','hyperbaric_chamber','bell_chamber','constant_supply', 'battery_supply','reclaim_system','diver_hot_water','environmental_control','potable_water','hp_compressor', 'launch_and_recovery',
+'touch_screen','ai_interface','machine_learning','three_right_turns', 'cooling_water',
+'pump_a','pump_b','gas_storage','helium_bottles','oxygen_bottles','treatment_bottles',
+'bell_bottles','hyperbaric_bottles','gas_distribution','bell_umbilical', 'oil_filled_box'
+ ]
