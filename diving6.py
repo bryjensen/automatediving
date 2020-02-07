66ab5c0c62bfd09f6959ef27a906fbb4902a4295
Bryan Jensen <56618606+bryjensen@users.noreply.github.com>
2020-02-06 20:28:17 -0800

    Add files via upload

--- diving6.py
+++ diving6.py
@@ -1,15 +1,71 @@
-#what is first, look at the chambers
-#make a virtual chamber first, easy to change the dimensions
+# what is first, look at the chambers
+# make a virtual chamber first, easy to change the dimensions
 # write out all the parts, its easy cause there is only a few
 # K.I.S.S.
+# Git Hub link-https://github.com/bryjensen/automatediving
+
+def machine_learning(constant, variable):
+    what is to be learned 
+    what is constant
+    what is variable
+    adjustments from outside set parameters, how to teach that 
+    the machine_learing will have a specific range to maintain with a variance to work with
+    setting new functions for distance from allowables, 
+    r0 is the function and r1 is the first distance to give directions about
+    twelve point two is ok but twelve point three is not so in order to maintain which steps are to be taken 
+    a balance between action and time for the reaction
+    reclaim_system machine learning based off of diver work through rates of exertion flow ratio of o2 to co2
+        volumes against volumes with which gateways controlling the normal range
+            diver mass with ranges of o2 consumption at depth 
+        
+def ai_interface(constant, variable):
+    teach the spam the why as to how the constant is maintained
+    avoid three_right_turns in snake
+    giving the ai_interface access to what to know what is going on#, toilet_flush will do what to the sensors
+    inputs = ['electrical','gas','hydraulics','highs and lows']
+    outputs = ['commands for the ai to give or how to give them'] 
+    # input(hi, low) input tells the ai oxygen level reaching a predetermined point to make a correction  
+    # 
+    
+def touch_screen(real, virtual):
+    # the interface what is going to be displayed for whom fingerprint access, dirty fingers
+    # offshore so open water, bracelet, something a person has to keep with them, location services show overboard scenarios security and safety
+    
+
 
 def main_chamber(constant, variable):
+    # sub section in section running with outputs at certain intervals or certain access to specific information
+    the internal_functions =
+    (what to put in here or how to do what in here)
+    inputs from sensors
+    output to what
+    main_chamber is taking inputs from which sensors, what sensors are doing what
+    what senses are being used for what 
+    temperature maintained from hot and cold water going in  
+    depth maintained from input of helium or exhausted 
+    electrical_connections
     electrical = # needs to be measured in which way, constant_supply, battery_supply
     heating/cooling = # central heating and cooling outside, larger volume to scrub
     emergency_procedures = # real and virtual
+    
 
 def transfer_chamber(constant, variable):
-    electrical = # needs to be measured in which way, constant_supply, battery_supply
+    electrical = { # how to seperate wires or watch them independently, ai controls through continuity checks(?)
+        inputs()
+        outputs()
+    }
+    potable_water = { # satellite to the
+        inputs()
+        outputs
+    }
+    gas = { 
+        press()
+        exhaust()
+    }
+    sample_lines = {
+        # where should what sensors be or have two, one inside the chamber with a traditional line supplying a sensor with the LST or sup 
+    }
+
 
 def hyperbaric_chamber(constant, variable):
     electrical = # needs to be measured in which way, constant_supply, battery_supply
@@ -19,29 +75,82 @@ def bell_chamber(constant, variable):
     # main_chamber(_a,b,c,...) for identification for added chambers
     
 def reclaim_system(constant, variable):
-    bell_chamber # the machine learning and ai for this is what is going to make something
-    main_chamber # everything else will be based off of this, need to draw some math
+
+    (bell_chamber # the machine learning and ai for this is what is going to make something
+        diver_reclaim # extension of the bell_chamber
+
+    )
+   
+   
+    (main_chamber # everything else will be based off of this, need to draw some math
+
+    )
+    
 
 def bell_umbilical(surface, bell):
+    the instantaneous holes
     
 def oil_filled_box(electrical, connections):
+    connection point for bell_umbilical to bell_chamber # how to monitor or do you want to empty the oil every time theres a problem
     
-def diver_hot_water(constant, variable):
+def diver_hot_water_a(constant, variable):
+    water in box heat it up water out
+def diver_hot_water_b(constant, variable):
     water in box heat it up water out
     
-def environmental_control(constants, variables):
+def environmental_control_a(constants, variables):
+    energy constant with high low spikes to control the temperature of a hot and cold glycol system
+
+def environmental_control_b(constants, variables):
+    operating parameters = [
+        power_supply = voltage
+        water_supply = cooling_water
+        glycol_supply_hot = 
+        glycol_supply_cold = 
+        compressor_needs = # sub category within operating parameters
+    ]
     
-def potable_water(constant, variables):
+def potable_water_a(constant, variables): # storage_depth, storage_depth plus overcome bottom_pressure. Make a link for
+    # specific constant
+    electricity in to cover the water out so what is water in
+    more power for deeper depths
+    ai_interface to learn pressure plus to overcome bottom_pressure 
+    
+def potable_water_b(constant, variables):  
+    hot_water()
+    cold_water() 
     
 def hp_compressor(constant, variable):
+    fill hp bottles with air not much excitement
+    # connecting hp_compressor to ai_interface(?)
+
+def main_air_a(for, initial_press):
+    two compressors pressing defined volume to approx 37fsw 
+def main_air_b(for, initial_press):
+    two compressors pressing defined volume to approx 37fsw 
+
+def initial_press(altitude, zero)or(?,?):
+
+def surface_diving(tender, crap):
+    no automation needed, tender_power lol
     
 def launch_and_recovery(constant, variable):
     hydraulics = # sensors for the machine_learning and ai_interface
+    touch_screen processes to handle what
+        launch = {  # connected pressed system
+            step1: turn hydraulic_power on #(low volt to high volt)
+            step2: time.py for specific checks on system both virtual and real to verify virtual
+            step3: a manual_choice must be done then comm to sup and sup proceeds when proper actions done 
+            step4: touch_screen option command choice, manual options that must be done in order for the virtual to take place
+            step5: this area for launch or recovery to be dependent upon system needs and company policy
+            step6: the variable 
+        
+        }
     
 def cooling_water(constant, variable):
     pump_a = online
     pump_b = offline
-    # watch the flow, minute adjustment from any restriction
+    # watch the flow, minute adjustment from any restriction filter/strainer, broken impeller, rusty pipe
     
 def gas_storage(max, variable):
     helium_bottles = # how many at max press
@@ -49,31 +158,27 @@ def gas_storage(max, variable):
     treatment_bottles = # how many at max press
     bell_bottles = # how many at max press
     hyperbaric_bottles = # how many at max press
-    other_bottles = # how many at max press
-    more_bottles = # how many at max press
+    reclaim_bags = # how many at max press
+    
     
 def gas_distribution(constants, variables):
     electrical_valves, more like a bike inner tube, inside keeps it closed, outside
-    will push open to allow needed amount, one ways that prevent instantaneous release
-    , spring resistance
+    will push open to allow needed amount, one ways that prevent instantaneous release, spring resistance
     intricities for the ai to manage, how many amps/watts to keep the valve open at which
     pressure, ability to manage a reducer 
 
-def machine_learning(constant, variable):
-    what is to be learned 
-    what is constant
-    what is variable
-    adjustments from outside set parameters, how to teach that 
-    
-def ai_interface(constant, variable):
-    teach the spam the why as to how the constant is maintained
-    avoid three_right_turns in snake
-    
-def touch_screen(real, virtual):
-    # the interface what is going to be displayed for whom
+def how_to_make_a_sensor_connection(what for variables):
+    power and neutral to sensor 
+    what is the signal
 
-list = ['main_chamber','transfer_chamber','hyperbaric_chamber','bell_chamber','constant_supply', 'battery_supply','reclaim_system','diver_hot_water','environmental_control','potable_water','hp_compressor', 'launch_and_recovery',
-'touch_screen','ai_interface','machine_learning','three_right_turns', 'cooling_water',
-'pump_a','pump_b','gas_storage','helium_bottles','oxygen_bottles','treatment_bottles',
-'bell_bottles','hyperbaric_bottles','gas_distribution','bell_umbilical', 'oil_filled_box'
+def sample_area(constant, variable):
+    # first sub category that encapulates sample_area makes an observation point, (ION point?)
+    # what or how does the ai_interface interact with sample_area
+    # what does sample_area need to do for inputs and outputs
+
+list = ['main_chamber','transfer_chamber','hyperbaric_chamber','bell_chamber','constant_supply','battery_supply','reclaim_system','diver_hot_water','environmental_control','potable_water','hp_compressor', 'launch_and_recovery',
+'touch_screen','ai_interface','machine_learning','three_right_turns','cooling_water','manual_choice'.'reclaim_bags'
+'pump_a','pump_b','gas_storage','helium_bottles','oxygen_bottles','treatment_bottles','toilet_flush','med_lock','equipment_lock'
+'bell_bottles','hyperbaric_bottles','gas_distribution','bell_umbilical','oil_filled_box','electrical_connections',
+'diver_reclaim','cold_water', 'hot_water',
  ]
